17ebd827f8fa80fdde0a989187e768e9f262c151
ansar <mert_karatutun@mail.ru>
2024-08-24 02:12:20 +0500

    modules

--- features/favourite/data/build.gradle.kts
+++ features/favourite/data/build.gradle.kts
@@ -37,6 +37,7 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
 
+    implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(projects.models.data)
     implementation(projects.models.domain)
     implementation(projects.models.mappers)
--- source/build.gradle.kts
+++ source/build.gradle.kts
@@ -36,6 +36,7 @@ dependencies {
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
     /**
      * Serialization
      */

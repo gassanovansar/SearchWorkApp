8fb58747a7e2a9df3fa2c45d248da19ca4b7a3f7
ansar <mert_karatutun@mail.ru>
2024-08-29 10:58:09 +0500

    change

--- app/src/main/java/com/example/searchworkapp/di/NetworkModule.kt
+++ app/src/main/java/com/example/searchworkapp/di/NetworkModule.kt
@@ -4,8 +4,10 @@ import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-val  networkModule = module {
-    single {   Retrofit.Builder().baseUrl("https://api.adampay.ru/")
-        .addConverterFactory(GsonConverterFactory.create())
-        .build() }
+val networkModule = module {
+    single {
+        Retrofit.Builder().baseUrl("https://api.adampay.ru/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
 }
